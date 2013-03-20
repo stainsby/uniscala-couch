fbd949269e3f23398796f0f82eef85a9a6d72bc2
Sam Stainsby <sam@stainsby.id.au>
2013-03-20 10:18:17 +1000

    use futures properly for ansynch behaviour when reading chunked content

--- src/main/scala/net/uniscala/couch/http/Content.scala
+++ src/main/scala/net/uniscala/couch/http/Content.scala
@@ -1,6 +1,6 @@
 package net.uniscala.couch.http
 
-import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.{future, ExecutionContext, Future, Promise}
 import scala.io.Source
 
 import java.io.{ByteArrayOutputStream, InputStream, Reader}
@@ -20,6 +20,9 @@ class Content private[http] (private val _source: Pipe.SourceChannel) {
   
   private def createReader: Reader = Channels.newReader(_source, "utf8")
   
+  // we need to use a Promise so that ResponseHandler can force failures on us
+  // by calling the 'failure' method
+  
   private def createPromise[T]: Promise[T] = synchronized {
     if (this.promise == null) {
       val prom = Promise[T]()
@@ -35,62 +38,51 @@ class Content private[http] (private val _source: Pipe.SourceChannel) {
     if (prom != null) prom.tryFailure(err)
   }
   
-  // TODO: put futures inside promises using Promise#tryCompleteWith?
-  
   def withStream[T](f: InputStream => T): Future[T] = {
     val prom = createPromise[T]
-    try {
-      prom.success(f(createStream))
-    } catch {
-      case err: Throwable => this.promise.failure(err)
-    }
+    prom.tryCompleteWith { future { f(createStream) } }
     prom.future
   }
   
   def withReader[T](f: Reader => T): Future[T] = {
     val prom = createPromise[T]
-    try {
-      prom.success(f(createReader))
-    } catch {
-      case err: Throwable => prom.failure(err)
-    }
+    prom.tryCompleteWith { future { f(createReader) } }
     prom.future
   }
   
   def withSource[T](f: scala.io.Source => T): Future[T] = {
     val prom = createPromise[T]
-    try {
-      prom.success(f(Source.fromInputStream((createStream))))
-    } catch {
-      case err: Throwable => prom.failure(err)
+    prom.tryCompleteWith {
+      future { f(Source.fromInputStream((createStream))) }
     }
     prom.future
   }
   
   def bytes: Future[Array[Byte]] = {
     val prom = createPromise[Array[Byte]]
-    try {
-      val src = _source
-      val out = new ByteArrayOutputStream(64*1024)
-      val dest = Channels.newChannel(out)
-      val buffer = java.nio.ByteBuffer.allocateDirect(8*1024);
-      
-      while (src.read(buffer) != -1) {
-        buffer.flip();
-        dest.write(buffer);
-        buffer.compact();
+    prom.tryCompleteWith {
+      future {
+        val src = _source
+        val out = new ByteArrayOutputStream(64*1024)
+        val dest = Channels.newChannel(out)
+        val buffer = java.nio.ByteBuffer.allocateDirect(8*1024);
+        
+        while (src.read(buffer) != -1) {
+          buffer.flip();
+          dest.write(buffer);
+          buffer.compact();
+        }
+        buffer.flip()
+        while (buffer.hasRemaining()) {
+          dest.write(buffer);
+        }
+        out.close
+        val res = out.toByteArray
+        try {
+          _source.close
+        }
+        res
       }
-      buffer.flip()
-      while (buffer.hasRemaining()) {
-        dest.write(buffer);
-      }
-      out.close
-      prom.success(out.toByteArray)
-      try {
-        _source.close
-      }
-    } catch {
-      case err: Throwable => prom.failure(err)
     }
     prom.future
   }
